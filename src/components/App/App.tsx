056b2c34890ef02597ae7dfd51549ff3a5065c26
RomanFS76 <korvy85@gmail.com>
2025-12-10 21:33:33 +0300

    add Pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,17 +2,24 @@ import { useQuery } from '@tanstack/react-query';
 import css from './App.module.css';
 import { fetchNotes } from '../../services/noteService';
 import NoteList from '../NoteList/NoteList';
+import Pagination from '../Pagination/Pagination';
+import { useState } from 'react';
 
 export default function App() {
-  const { data } = useQuery({ queryKey: ['notes'], queryFn: fetchNotes });
+  
+  const [page, setPage] = useState(1);
+
+  const { data } = useQuery({ queryKey: ['notes',page], queryFn: fetchNotes });
 
   const note = data?.notes ?? [];
+  const totalPages = data?.totalPages ?? 0;
 
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
-        <NoteList notes={note} />
+        {totalPages > 1 && <Pagination totalPages={totalPages} page={page} onChangePage={setPage}/>}
       </header>
+      {note.length > 0 && <NoteList notes={note} />}
     </div>
   );
 }
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,5 +1,27 @@
-const Pagination = () => {
-  return <div>Pagination</div>;
+import ReactPaginate from 'react-paginate';
+import css from"./Pagination.module.css";
+
+interface paginationProps {
+  totalPages: number,
+  page:number,
+  onChangePage:(page: number)=>void
+}
+
+
+const Pagination = ({ totalPages, page,onChangePage}:paginationProps) => {
+  return (
+    <ReactPaginate
+      pageCount={totalPages}
+      pageRangeDisplayed={5}
+      marginPagesDisplayed={1}
+      onPageChange={({ selected }) => onChangePage(selected + 1)}
+      forcePage={page - 1}
+      containerClassName={css.pagination}
+      activeClassName={css.active}
+      nextLabel="→"
+      previousLabel="←"
+    />
+  );
 };
 
 export default Pagination;
