da6e6d571c56476de8cd454c51234e65720f5b6c
RomanFS76 <korvy85@gmail.com>
2025-12-10 19:40:39 +0300

    add useQuery in App, NoteList

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
 import css from './App.module.css';
+import { fetchNotes } from '../../services/noteService';
+import NoteList from '../NoteList/NoteList';
 
 export default function App() {
+  const { data } = useQuery({ queryKey: ['notes'], queryFn: fetchNotes });
+
+  const note = data?.notes ?? [];
+
   return (
-    <>
-      <div className={css.app}>Hello</div>
-    </>
+    <div className={css.app}>
+      <header className={css.toolbar}>
+        <NoteList notes={note} />
+      </header>
+    </div>
   );
 }
--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,9 +1,23 @@
-import css from './App.module.css';
+import type { Note } from '../../types/note';
+import css from './NoteList.module.css';
 
-export default function App() {
+interface notesProps {
+  notes: Note[];
+}
+
+export default function NoteList({notes}: notesProps) {
   return (
-    <>
-      <div className={css.app}>Hello</div>
-    </>
+    <ul className={css.list}>
+      {notes.map((note) => (
+        <li className={css.listItem}>
+          <h2 className={css.title}>{note.title}</h2>
+          <p className={css.content}>{note.content}</p>
+          <div className={css.footer}>
+            <span className={css.tag}>{note.tag}</span>
+            <button className={css.button}>Delete</button>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
