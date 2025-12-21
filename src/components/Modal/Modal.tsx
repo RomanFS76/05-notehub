3fa8fc9f65c446304b305e619b07d77c606218d1
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2025-12-21 16:04:50 +0200

    add to Modal  event overflow

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -22,9 +22,11 @@ export default function Modal({ children, onClose }: ModalProps) {
     };
 
     document.addEventListener('keydown', handleKeyDown);
+    document.body.style.overflow = "hidden";
 
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
+      document.body.style.overflow = "";
     };
   }, [onClose]);
 
